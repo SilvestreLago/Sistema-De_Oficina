be9b0e8df12bb92c10689d095e81a077b0d116f4
Silvestre <silvestrelago@protonmail.com>
2025-02-16 20:47:59 -0300

    adicionando historico

--- pag/caixa.php
+++ pag/caixa.php
@@ -1,20 +1,24 @@
 <?php include_once "main.php";?>
 <div class="container formulario">
+    <div class="titulo">
+        <a class="btn btn-secondary" href="caixa.php" role="button" >Cadastrar</a>
+        <a class="btn btn-primary" href="historico.php" role="button">Ver</a>
+    </div>
     <h1>Caixa:</h1>
     <div class="mb-3">
         <h2>R$00,00</h2>
     </div>
-    <form style="padding: 1%;">
+    <form>
         <div class="mb-3">
             <label for="exampleInputDinheiro1" class="form-label">Adicionar R$:</label>
             <input type="number" class="form-control" id="exampleInputDinheiro1" aria-describedby="dinheiroHelp" placeholder="R$00.00">
         </div>
         <button type="submit" class="btn btn-primary">Adicionar</button> 
     </form>
-    <form action="" style="padding: 1%;">
+    <form style="padding: 1% 0%;">
         <div class="mb-3">
-            <label for="exampleInputQuantidade2" class="form-label">Quantidade:</label>
-            <input type="number" class="form-control" id="exampleInputQuantidade2" aria-describedby="quantidadeHelp" placeholder="0">
+            <label for="exampleInputQuantidade2" class="form-label">Remover:</label>
+            <input type="number" class="form-control" id="exampleInputQuantidade2" aria-describedby="removerHelp" placeholder="R$00.00">
         </div>
         <button type="submit" class="btn btn-primary">Remover</button> 
     </form>
---
+++ pag/historico.php
@@ -0,0 +1,39 @@
+<?php include_once 'main.php';?>
+<div class="container formulario">
+    <div class="titulo">
+        <a class="btn btn-secondary" href="caixa.php" role="button" >Cadastrar</a>
+        <a class="btn btn-primary" href="historico.php" role="button">Ver</a>
+    </div>
+    <table class="table table-hover table-bordered">
+    <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Data:</th>
+          <th scope="col">Valor:</th>
+          <th scope="col">Ver histórico:</th>
+        </tr>
+      </thead>
+      <tbody class="table-group-divider">
+        <tr>
+            <th scope="row">1</th>
+            <td>09/02/2025</td>
+            <td>R$300.00</td>
+            <td>
+                <a href="" style="text-decoration: none; color: blue;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16"><path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/><path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/></svg>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>08/02/2025</td>
+            <td>R$250.00</td>
+            <td>
+                <a href="" style="text-decoration: none; color: blue;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16"><path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/><path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/></svg>
+                </a>
+            </td>
+        </tr>
+      </tbody>
+    </table>
+</div>
