ad4809da1a9630414774c81e4e7cc1811fa4eaed
Silvestre <silvestrelago@protonmail.com>
2025-04-05 18:00:08 -0300

    update

--- js/verEstoque.js
+++ js/verEstoque.js
@@ -1,4 +1,4 @@
-//SISTEMA SEARCH AS YOU TYPE ESTOQUE
+// SISTEMA SEARCH AS YOU TYPE ESTOQUE
 document.getElementById('pesquisa').addEventListener('keyup', function() {
     let termo = this.value.trim();
     if (termo.length < 2) {
@@ -14,11 +14,16 @@ document.getElementById('pesquisa').addEventListener('keyup', function() {
                 return;
             }
 
+            function removerAcentos(str) {
+                return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            }
+
             let html = '<ul class="list-group" style="padding: 1%;">';
             data.forEach(produto => {
+                let nomeSemAcento = removerAcentos(produto.nome);
                 html += `
                     <li class="list-group-item">
-                        <a href="verEstoque.php?nome=${produto.nome}"><strong>${produto.nome}</strong></a><br>
+                        <a href="verEstoque.php?nome=${encodeURIComponent(nomeSemAcento)}"><strong>${nomeSemAcento}</strong></a><br>
 
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"/></svg>${produto.quantidade}
                     </li>`;
